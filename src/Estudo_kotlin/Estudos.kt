432fc7e1b0ddff80a5b192a1d09a65f9ed1e988b
antonio <antg3102@gmail.com>
2025-08-09 13:02:24 -0300

    Primeiro commit

---
+++ src/Estudo_kotlin/Estudos.kt
@@ -0,0 +1,8 @@
+package Estudo_kotlin
+
+fun main() {
+    val name = "Mary"
+    val age = 20
+
+    println("$name is $age Years old")
+}
---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+
+fun main() {
+    val clientes = 10
+    println("Existem ${clientes} clientes")
+    println("Existem ${clientes + 1} clientes")
+}
