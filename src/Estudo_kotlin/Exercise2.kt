e69096a4e940636c92fcd4f4e218d4a309a2ee46
antonio <antg3102@gmail.com>
2025-08-10 00:26:50 -0300

    Exercise to set

---
+++ src/Estudo_kotlin/Exercise2.kt
@@ -0,0 +1,9 @@
+package Estudo_kotlin
+
+fun main(){
+        val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
+        val requested = "smtp"
+        val isSupported = requested.uppercase() in SUPPORTED
+            println("Support for $requested: $isSupported")
+
+}
